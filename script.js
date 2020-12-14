6c70f64bf866e6eab4e692c969f0b869c2982ace
Coffee Addict <shikh.alshabab@gmail.com>
2020-12-14 17:51:12 +0300

    add Messages object with three arrays

---
+++ script.js
@@ -0,0 +1,10 @@
+const Messages= {
+    m1:["sun.", "moon", "earth", "jupiter", "venus", "mercury"],
+    m2:["good luck.", "bad omen", "graet week", "surprising event"],
+    m3:["trust no one", "be positive", "be careful", "increase your awareness"]
+}
+
+for(let key in Messages)
+{
+    
+}
