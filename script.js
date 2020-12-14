204c61d99533c1d52cbbc758f031b1c3e5ecd518
Coffee Addict <shikh.alshabab@gmail.com>
2020-12-14 18:18:10 +0300

    output rand msg

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const Messages= {
-    m1:["sun.", "moon", "earth", "jupiter", "venus", "mercury"],
-    m2:["good luck.", "bad omen", "graet week", "surprising event"],
-    m3:["trust no one", "be positive", "be careful", "increase your awareness"]
+const Messages = {
+  m1: ["sun.", "moon", "earth", "jupiter", "venus", "mercury"],
+  m2: ["good luck.", "bad omen", "great week", "surprising event"],
+  m3: ["trust no one", "be positive", "be careful", "increase your awareness"],
+};
+
+let message = "";
+for (let key in Messages) {
+  switch (key) {
+    case "m1":
+      message +=
+        "Your sign is: " +
+        Messages[key][Math.floor(Math.random() * Messages[key].length)] +
+        "\n";
+      break;
+    case "m2":
+      message +=
+        "You are having a " +
+        Messages[key][Math.floor(Math.random() * Messages[key].length)] +
+        "\n";
+      break;
+    case "m3":
+      message +=
+        "You should " +
+        Messages[key][Math.floor(Math.random() * Messages[key].length)];
+      break;
+    default:
+      message = "no message";
+      break;
+  }
 }
 
-for(let key in Messages)
-{
-    
-}
+console.log(message);
